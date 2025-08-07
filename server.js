797388eaed400d4315b008a16dc091df2b058d46
Arka <arkapravasantra17@gmail.com>
2025-08-07 23:24:08 +0530

    Mecadoor

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const app = require('./src/app')
+const app = require("./src/app");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
-
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+const io = new Server(httpServer, {
+  /* options */
+});
 
 io.on("connection", (socket) => {
   // ...
+  console.log("A user Connected");
+  socket.on("disconnect", () => {
+    console.log("A user is Disconnected");
+  });
+  socket.on("message",(data)=>{
+    console.log(data)
+    
+  })
 });
 
-httpServer.listen(3000,()=>{
-  console.log("Console is running")
-})
-
+httpServer.listen(3000, () => {
+  console.log("Console is running on 3000");
+});
